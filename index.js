7a2c7cb5be60bb759fcb04e4c746fbdb21168b77
matt <matt.dennis@gmail.com>
2018-04-05 16:56:24 -0400

    .

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var express = require('express')
+const sha1 = require('sha1')
+
+//express is the webserver
+const express = require('express')
+
+//crpto is how we decrypt
 const crypto = require('crypto');
 
+//you have to do this to set it up to be a server
 var app = express()
 
+//tell it to listen on a port
 app.set('port', (process.env.PORT || 5000))
+
+//this tells it which directory to use as the base for files and stuff
 app.use(express.static(__dirname + '/public'))
 
+//when someone makes a GET request to /   it runs this function and sends the payload
 app.get('/', function(request, response) {
 
 
@@ -15,6 +25,10 @@ app.get('/', function(request, response) {
 	payload = request.query.data;
 	//payload is encrypted ascii json
 
+	const salt = 'truth of source';
+
+	const valid = sha1(payload + salt) === request.query.hash;
+	console.log(valid);
 	//for some weird reason, i had to add a few characters to make it work properly
 	payload += 'aaaa';
 
@@ -25,6 +39,7 @@ app.get('/', function(request, response) {
 
 })
 
+//this is how you start it up
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
